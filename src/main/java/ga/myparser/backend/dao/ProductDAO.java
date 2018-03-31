3c8d7dc773dc83e90579e64629957d162b18886d
Ivan Alexeev <alexeevanya89@gmail.com>
2018-03-31 21:32:02 +0300

    update DAO

--- src/main/java/ga/myparser/backend/dao/ProductDAO.java
+++ src/main/java/ga/myparser/backend/dao/ProductDAO.java
@@ -2,7 +2,11 @@ package ga.myparser.backend.dao;
 
 import ga.myparser.backend.domain.Product;
 
+import java.util.List;
+
 public interface ProductDAO {
 
     void save(Product product);
+
+    List<Integer> getMyIdByFRId(int frId);
 }
--- src/main/java/ga/myparser/backend/dao/impl/ProductDAOImpl.java
+++ src/main/java/ga/myparser/backend/dao/impl/ProductDAOImpl.java
@@ -5,7 +5,11 @@ import ga.myparser.backend.domain.Product;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.Collections;
+import java.util.List;
 
 @Repository
 public class ProductDAOImpl implements ProductDAO {
@@ -16,5 +20,17 @@ public class ProductDAOImpl implements ProductDAO {
     @Override
     public void save(Product product) {
         em.persist(product);
+    }
+
+    @Override
+    public List<Integer> getMyIdByFRId(int frId) {
+        Query query = em.createQuery("select m.myProductId from MyProductIdToFreeRunId m where m.freRunProductId = :frId");
+        query.setParameter("frId", frId);
+
+        try {
+            return (List<Integer>) query.getResultList();
+        } catch (NoResultException e){
+            return Collections.emptyList();
+        }
     }
 }
