caa525796218439537875e6eb227f8e6234070db
Ivan Alexeev <alexeevanya89@gmail.com>
2018-04-03 00:37:42 +0300

    added form action

--- src/main/java/ga/myparser/backend/controller/mvc/RootController.java
+++ src/main/java/ga/myparser/backend/controller/mvc/RootController.java
@@ -1,16 +1,23 @@
 package ga.myparser.backend.controller.mvc;
 
 import ga.myparser.backend.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Controller
 public class RootController {
 
@@ -18,15 +25,28 @@ public class RootController {
     ProductService productService;
 
     @RequestMapping("/")
-    public String index() throws IOException {
+    public String index() {
 
-        Document listCategory = Jsoup.connect("http://free-run.kiev.ua/catalog/adidas-gazelleneo").get();
+        return "index";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String parseCetegory(Model model, @RequestParam String categoryURL){
+
+        Document listCategory = null;
+        try {
+            listCategory = Jsoup.connect(categoryURL).get();
+        } catch (IOException e) {
+            log.warn("Can't parse URL {}", categoryURL);
+        }
 
         List<String> listCategoryToParse = productService.getListCategoryToParse(listCategory);
 
         ArrayList<String> listProductsToParse = productService.getListProductsToParse(listCategoryToParse);
 
         productService.parseProducts(listProductsToParse);
+
+        model.addAttribute("listProductsToParse", listProductsToParse);
 
         return "index";
     }
